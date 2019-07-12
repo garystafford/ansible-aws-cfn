1e0f784e561bd087cd833cb54443e2104d5bbecf
Gary A. Stafford <garystafford@rochester.rr.com>
2019-07-11 22:46:49 -0400

    Refactor project and scripts

---
+++ code_build/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export BUCKET_PATH
+
+for filename in ../templates/*.*;
+    do cfn-lint -t ${filename};
+    aws cloudformation validate-template --template-body file://${filename};
+    aws s3 cp ${filename} s3://${BUCKET_PATH}
+done
