2f39e2a24d7f21802fa4a98828de34401f138c22
Gary A. Stafford <garystafford@rochester.rr.com>
2019-07-14 09:57:23 -0400

    Refactor script(s)

---
+++ README.md
@@ -0,0 +1,3 @@
+# CloudFormation Templates for Ansible AWS Demo
+
+Contains the CloudFormation Templates and CodeBuild buildspec file for the upcoming post, 'Managing AWS Infrastructure as Code using Ansible, CloudFormation, and CodeBuild'.
--- code_build/build.sh
+++ code_build/build.sh
@@ -4,8 +4,9 @@
 
 export BUCKET_PATH
 
-for filename in templates/*.*;
-    do cfn-lint -t ${filename};
+for filename in templates/*.*
+do
+    cfn-lint -t ${filename};
     aws cloudformation validate-template --template-body file://${filename};
     aws s3 cp ${filename} s3://${BUCKET_PATH}
 done
