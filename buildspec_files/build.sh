a70066c459bb4f70f41828fa3b58bf2a494053c0
Gary A. Stafford <garystafford@rochester.rr.com>
2019-07-14 10:59:59 -0400

    Refactor scripts

--- buildspec_files/build.sh
+++ buildspec_files/build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-#set -ex
+set -e
 
 export BUCKET_PATH
 
+# lint all templates
 for filename in cfn_templates/*.*
 do
     cfn-lint -t ${filename};
     aws cloudformation validate-template --template-body file://${filename};
+done
+
+# copy all templates if all linting passes
+for filename in cfn_templates/*.*
+do
     aws s3 cp ${filename} s3://${BUCKET_PATH}
 done
--- codebuild_templates/build.sh
+++ codebuild_templates/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#set -ex
+set -e
 
 for template in ./*.json
 do
