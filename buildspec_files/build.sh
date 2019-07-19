5c9f4040cf9bb10ba8bdc9b6f77d92ed5035d2e8
Gary A. Stafford <garystafford@rochester.rr.com>
2019-07-18 23:33:50 -0400

    Refactor scripts

--- buildspec_files/build.sh
+++ buildspec_files/build.sh
@@ -3,6 +3,6 @@
 set -e
 
 for filename in cfn_templates/*.*; do
-    cfn-lint -t ${filename};
+    cfn-lint -t ${filename} -i W1020 W2030;
     aws cloudformation validate-template --template-body file://${filename};
 done
